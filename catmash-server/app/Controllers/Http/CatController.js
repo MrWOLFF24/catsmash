87c96412bdc7325cc2980db5193c37ee163efff9
mac <mac@mac.local>
2019-06-09 20:08:10 +0200

    🐱 update cats table after battle result with loser and winner data

--- catmash-server/app/Controllers/Http/CatController.js
+++ catmash-server/app/Controllers/Http/CatController.js
@@ -19,17 +19,25 @@ class CatController {
     }
   }
 
+  // update cat informations after battle
   async updateBattleResult({ request }) {
-    return await Cats.findById(request.id)
+    const { winner, loser } = request.all()
+    const cats = await Database.from('cats').whereIn('id', [winner.id, loser.id])
+    const ranking = this._getRating(winner, loser)
+    const affectedRowWinner = await Database.table('cats').where('id', cats[0].id).update({ ranking: ranking.winner, wins: cats[0].wins += 1 })
+    const affectedRowLoser = await Database.table('cats').where('id', cats[1].id).update({ ranking: ranking.loser, losses: cats[0].losses += 1 })
+    return await {
+      message: 'Thanks for your vote, cats will get more power!!!'
+    }
   }
 
-  async getRating(winner, loser) {
-    let k = 30, winnerExpected, loserExpected
+  _getRating(winner, loser) {
+    let k = 30, winnerExpected, loserExpected;
 
     winnerExpected = 1 / (1 + (Math.pow(10,(loser.ranking - winner.ranking) / 400)))
     loserExpected = 1 / (1 + (Math.pow(10,(winner.ranking - loser.ranking) / 400)))
 
-    return await {
+    return {
       winner: Math.round(winner.ranking + (k * (1 - winnerExpected))),
       loser: Math.round(loser.ranking + (k * (0 - loserExpected)))
     }
